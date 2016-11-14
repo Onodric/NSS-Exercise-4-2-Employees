760786139590db3e066484bc48d89da33b11509e
Ben Marks <onodric@yahoo.com>
2016-11-13 21:21:54 -0600

    Questions about depth of prototype and number of required elements

--- javascripts/employees.js
+++ javascripts/employees.js
@@ -1,42 +1,3 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
-
-let Dept = function () {
-  this.name = '';
-  this.location = '';
-};
-
-Dept.prototype.setName = function (name) {
-  this.name = name;
-};
-
-Dept.prototype.setLocation = function (loc) {
-  this.location = loc;
-};
-
-Dept.FieldOffice = function (name, loc) {
-  this.setName(name);
-  this.setLocation(loc);
-};
-
-Dept.FieldOffice.prototype = new Dept();
-
-// Create a two-level prototype chain for creating departments in your business. Start off with a `Department` function. Then create two other functions named `FieldOffice`, and `Headquarters` that have `Department` on their prototype chain.
-
-// 1. Every employee should have a first and last name.
-// 1. Every department should have a name.
-// 1. Every employee should be assigned to either the `FieldOffice` or `Headquarters` department. This is a `has-a` relationship.
-// 1. `Clinician` employees should have a property for their area of medical expertise (e.g. physical therapist, phrenologist, anesthesiologist, etc.)
-// 1. `Operations` employees should have a property for their area of responsibility (e.g. IT, revenue, compliance, etc.)
-// 1. Every field office should have a unique city.
-// 1. Create a minimum of 4 employees - 2 clinicians, and 2 operations - that have been assigned to a specific department.
-// 1. Output the following information to the console for each employee.
-//     1. Full name
-//     1. Department name
-//     1. Expertise/responsibility
-
-module.exports = Dept;
-},{}],2:[function(require,module,exports){
 "use strict";
 
 var Dept = require('./departments.js');
@@ -120,16 +81,3 @@ fo04561.toLog();
 //     1. Expertise/responsibility
 
 module.exports = {};
-},{"./departments.js":1}],3:[function(require,module,exports){
-"use strict";
-
-var Employ = require("./employees.js");
-var Depart = require("./departments.js");
-
-// Deborah Harris works as a clinician, in the Baltimore field office, and is a phrenologist.
-// Michael Slazinski works in operations, in headquarters, and is responsible for compliance.
-// Margaret Johnson works as a clinician, in the Indianapolis field office, and is a physical therapist.
-
-var Business = function(){};
-
-},{"./departments.js":1,"./employees.js":2}]},{},[3]);
